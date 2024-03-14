3a9dc429f2a091a4615c54579925e52cc1feab41
Rickiexxlin <rickie_lin@outlook.com>
2024-03-14 17:08:38 -0400

    Add DJI_Index data

--- data/code/yahoo.py
+++ data/code/yahoo.py
@@ -3,8 +3,8 @@ import yfinance as yf
 from yahoofinancials import YahooFinancials
 
 DJI_df = yf.download('DJI', 
-                      start='2008-08-08', 
-                      end='2016-01-01', 
+                      start='2017-11-01', 
+                      end='2020-06-04', 
                       progress=False,
 )
 
@@ -13,4 +13,4 @@ DJI_df = yf.download('DJI',
 # Save the data to a CSV file
 DJI_df = DJI_df[['Adj Close']]
 # print(DJI_df.head())
-DJI_df.to_csv('data/DJI_08-08-2008_01-01-2016_prices.csv')
+DJI_df.to_csv('./data/DJI_17to20_prices.csv')
