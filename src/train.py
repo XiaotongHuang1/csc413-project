192d27be83eb4cbe0d6b4397d1c8b4a96267a970
xiaotonghuang <xiaotong.huang@mail.utoronto.ca>
2024-03-14 15:21:22 -0400

    add basic starter code

---
+++ src/train.py
@@ -0,0 +1,4 @@
+from transformers import BertTokenizer, BertForSequenceClassification
+
+finbert = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone',num_labels=3)
+tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
